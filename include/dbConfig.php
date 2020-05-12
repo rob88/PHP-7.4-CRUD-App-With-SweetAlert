9c61e0400da1b6390c72b01c7b2d68244f339b02
Rob88 <Reben_f@hotmail.co.uk>
2020-05-12 03:03:59 +0100

    creating config file to connect to database

---
+++ include/dbConfig.php
@@ -0,0 +1,39 @@
+<?php
+
+class Banco
+{
+   
+    private static $dbHost = 'localhost';
+    private static $dbUser = 'root';
+    private static $pass = '';
+    private static $dbName = 'course_db';
+    private static $cont = null;
+    
+    public function __construct() 
+    {
+        die('Connection issue!');
+    }
+    
+    public static function connector()
+    {
+        if(null == self::$cont)
+        {
+            try
+            {
+                self::$cont =  new PDO( "mysql:host=".self::$dbHost.";"."dbname=".self::$dbName, self::$dbUser, self::$pass); 
+            }
+            catch(PDOException $exception)
+            {
+                die($exception->getMessage());
+            }
+        }
+        return self::$cont;
+    }
+    
+    public static function disconnect()
+    {
+        self::$cont = null;
+    }
+}
+
+?>
